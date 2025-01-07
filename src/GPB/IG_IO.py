9b844e5988ac3fd4fe334382172fdf0e750b4222
MarcoGrossi92 <marcogrossi@live.it>
2025-01-08 00:41:22 +0100

    Fix efficiency coefficient retrieval for third-body reactions in write_chemistry_properties function

--- src/GPB/IG_IO.py
+++ src/GPB/IG_IO.py
@@ -191,12 +191,11 @@ def write_chemistry_properties (name, T_low, T_max, phase, further_sp):
                         product_coeff = coeff
                     # Print third-body efficiencies if applicable
                     if 'three-body' in reaction.reaction_type or 'falloff' in reaction.reaction_type:
-                        # Get explicitly specified efficiencies
-                        specified_efficiencies = reaction.efficiencies
+                        specified_efficiencies = reaction.third_body.efficiencies
                         if species_name in specified_efficiencies:
                             efficiency_coeff = specified_efficiencies[species_name]
                         else:
-                            efficiency_coeff = reaction.efficiency(species_name)
+                            efficiency_coeff = reaction.third_body.efficiency(species_name)
                 
                 file.write(f'{i + 1} {species_name} {reactant_coeff} {product_coeff} {efficiency_coeff}\n')
 
