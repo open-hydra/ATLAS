2157ba93a709eb37ffc13d37d452a87fa6573d99
MarcoGrossi92 <marcogrossi@live.it>
2025-03-10 16:39:16 +0100

    bug fix: add HG factor adjustment for molecular weights in thermo properties calculation

--- src/GPB/Ideal_Gas.py
+++ src/GPB/Ideal_Gas.py
@@ -304,11 +304,7 @@ def build(inifile,section):
             all_species = [sp for sp in solution.species() if "(L)" not in sp.name]
             cond_species = [sp for sp in solution.species() if "(L)" in sp.name]
             for sp in cond_species:
-                HG_species = ct.Species(name=sp.name + '-HG', composition={'H': 1e+5 * sp.molecular_weight / 1.008}, 
-                                        thermo=None, transport=None)
-                HG_species.thermo = sp.thermo
-                if sp.transport is not None: HG_species.transport = sp.transport
-                all_species.append(HG_species)
+                all_species.append(sp)
             new_phase = ct.Solution(thermo=solution.thermo_model,species=all_species, kinetics=solution.kinetics_model)
             new_phase.Y = solution.Y
         else:
