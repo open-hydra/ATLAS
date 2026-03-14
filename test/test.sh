91cfc237c841ec2ad3e7c6bbce38e33406040c30
Marco Grossi <50102629+MarcoGrossi92@users.noreply.github.com>
2026-03-14 23:13:17 +0100

    manage tests

--- test/test.sh
+++ test/test.sh
@@ -37,7 +37,7 @@ for CODE in $(ls -d */); do
   for TEST in $(ls -d */); do
     echo $TEST >> $FILE
     cd $TEST
-    [ $@ == clean ] && rm -rf error* fromATLAS* KAnT-out* *.out
+    [ $@ == clean ] && rm -rf error* fromATLAS* KAnT-out* *.out points.txt couples.txt volumes.txt
     if [[ $@ == run ]]; then
       $ATLASDIR/ATLAS.sh ${CODE%?} 1>>$FILE 2>error_file
       size=$(wc -c error_file | awk '{print $1}')
