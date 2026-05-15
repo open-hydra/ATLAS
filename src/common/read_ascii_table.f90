ee3ff148bccde651980d9bddc8c476c316ea86bc
Marco Grossi <50102629+MarcoGrossi92@users.noreply.github.com>
2026-05-15 12:59:18 +0200

    minor change to make it more general

--- src/common/read_ascii_table.f90
+++ src/common/read_ascii_table.f90
@@ -1,4 +1,5 @@
 module io_ascii_table_mod
+  use global_mod, only: llen
   implicit none
   private
   public :: read_ascii_table
@@ -10,6 +11,8 @@ contains
     character(len=*), intent(in) :: varfile
     real(8), dimension(:), allocatable, intent(out) :: file_dir, file_var
     integer :: ios, file_length, i, unitfile, error
+    character(len=10*llen) :: line
+    real(8) :: tmp_dir, tmp_var
 
     file_length=0; error=0
     open(newunit=unitfile,file=trim(varfile),status='old',action='read',iostat=ios)
@@ -17,17 +20,43 @@ contains
       error = ios
       return
     endif
-    do while (ios==0)
-      read(unitfile,*,iostat=ios)
-      file_length = file_length+1
+    do
+      read(unitfile,'(A)',iostat=ios) line
+      if (ios /= 0) exit
+      if (len_trim(line) == 0) cycle
+      if (line(1:1) == '#' .or. line(1:1) == '!') cycle
+      read(line,*,iostat=ios) tmp_dir, tmp_var
+      if (ios == 0) file_length = file_length+1
+      ios = 0
     enddo
-    file_length = file_length-1
+
+    if (file_length <= 0) then
+      close(unitfile)
+      error = -1
+      return
+    endif
+
     rewind(unitfile)
     allocate(file_dir(1:file_length))
     allocate(file_var(1:file_length))
-    do i = 1, file_length
-      read(unitfile,*) file_dir(i), file_var(i)
+    i = 0
+    do
+      read(unitfile,'(A)',iostat=ios) line
+      if (ios /= 0) exit
+      if (len_trim(line) == 0) cycle
+      if (line(1:1) == '#' .or. line(1:1) == '!') cycle
+      read(line,*,iostat=ios) tmp_dir, tmp_var
+      if (ios /= 0) cycle
+      i = i + 1
+      if (i > file_length) exit
+      file_dir(i) = tmp_dir
+      file_var(i) = tmp_var
     enddo
+
+    if (i /= file_length) then
+      error = -2
+    endif
+
     close(unitfile)
   end subroutine read_ascii_table
 
